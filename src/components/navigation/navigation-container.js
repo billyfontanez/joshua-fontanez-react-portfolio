f455f8091f1e3b5dee4688373de1dd4617070dc8
William Fontanez <william.r.fontanez@gmail.com>
2022-03-01 15:08:59 -0600

    Finish regular navigation bar functionality

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom'
 
 export default class NavigationComponent extends Component {
     constructor(){
@@ -8,11 +9,10 @@ export default class NavigationComponent extends Component {
     render() {
         return (
             <div>
-                <button>Home</button>
-                <button>About</button>
-                <button>Contact</button>
-                <button>Blog</button>
-                {true ? <button>Add Blog</button> : null}
+                <NavLink exact to='/'>Home</NavLink>
+                <NavLink to='/about-me'>About</NavLink>
+                <NavLink to='/contact'>Contact</NavLink>
+                <NavLink to='/blog'>Blog</NavLink>
             </div>
         )
     }
